21a41cba92c24e4aff985ca9326c6dbc759374e8
sarojregmi200 <sarojregmi.official@gmail.com>
2023-08-12 16:24:09 +0545

    Update: created the baisc information logger

---
+++ server.js
@@ -0,0 +1,51 @@
+import express from "express";
+import useragent from "useragent";
+import { publicIpv4 } from "public-ip";
+import fs from "fs/promises";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  try {
+    const userAgent = useragent.parse(req.headers["user-agent"]);
+    const ipAddress = await publicIpv4();
+    const timestamp = new Date().toISOString();
+    const data = {
+      timestamp,
+      browser: userAgent.toAgent(),
+      system: `${userAgent.os.toString()} ${userAgent.os.version}`,
+      timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+      ipAddress,
+    };
+
+    const logEntry = `
+      Timestamp: ${data.timestamp}
+      Location: ${data.location}
+      Browser: ${data.browser}
+      System: ${data.system}
+      Timezone: ${data.timeZone}
+      IP Address: ${data.ipAddress}
+      -----------------------------------
+    `;
+
+    fs.appendFile("./log.md", logEntry, (err) => {
+      if (err) {
+        console.error("Error writing to log file:", err);
+      } else {
+        console.log("Visitor information logged.");
+      }
+    });
+
+    res.status(200).send("Visitor information logged.");
+  } catch (error) {
+    console.error("Error processing request:", error);
+    res.status(500).send("Error processing request.");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
