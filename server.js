61a12d4c7f9375b6324f7e271963eef5c685431d
sarojregmi200 <sarojregmi.official@gmail.com>
2023-09-02 23:58:42 +0545

    ipaddr: test2

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 import express from "express";
 const port = 3000;
 
+const app = express();
 app.use(express.json());
 app.set('trust proxy', true);
 
 app.get("/", async (req, res) => {
- console.log(req.ip)
+
+    let ips = (
+        req.headers['cf-connecting-ip'] ||
+        req.headers['x-real-ip'] ||
+        req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress || ''
+    ).split(',');
+
+    console.log(ips[0].trim());
+
 });
 
 app.listen(port, () => {
