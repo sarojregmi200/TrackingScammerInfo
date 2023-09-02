5134a94b22ecf544d0d7d298fa8697c11037892d
sarojregmi200 <sarojregmi.official@gmail.com>
2023-09-02 23:47:09 +0545

    ipaddr: testing

---
+++ log.md
@@ -0,0 +1,15 @@
+Timestamp: 2023-08-12T11:24:53.268Z
+Aug 12 05:09:53 PM Location: undefined
+Aug 12 05:09:53 PM Browser: Mobile Safari 16.6.0
+Aug 12 05:09:53 PM System: iOS 16.6.0 undefined
+Aug 12 05:09:53 PM Timezone: UTC
+Aug 12 05:09:53 PM IP Address: 54.191.253.12
+
+      Timestamp: 2023-08-12T11:46:48.521Z
+      Location: undefined
+      Browser: Firefox 116.0.0
+      System: Ubuntu 0.0.0 undefined
+      Timezone: Asia/Katmandu
+      IP Address: 45.123.223.246
+      client Ip: ::ffff:127.0.0.1
+      -----------------------------------
--- server.js
+++ server.js
@@ -1,56 +1,11 @@
 import express from "express";
-import useragent from "useragent";
-import { publicIpv4 } from "public-ip";
-import fs from "fs/promises";
-
-const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.set('trust proxy', true);
 
 app.get("/", async (req, res) => {
-  try {
-    const userAgent = useragent.parse(req.headers["user-agent"]);
-    const ipAddress = await publicIpv4();
-    const timestamp = new Date().toISOString();
-
-    const clientIpAddress = req.ip;
-    const data = {
-      timestamp,
-      browser: userAgent.toAgent(),
-      system: `${userAgent.os.toString()} ${userAgent.os.version}`,
-      timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-      ipAddress,
-      clientIpAddress,
-    };
-
-    const logEntry = `
-      Timestamp: ${data.timestamp}
-      Location: ${data.location}
-      Browser: ${data.browser}
-      System: ${data.system}
-      Timezone: ${data.timeZone}
-      IP Address: ${data.ipAddress}
-      client Ip: ${data.clientIpAddress}
-      -----------------------------------
-    `;
-
-    console.log(logEntry);
-
-    fs.appendFile("./log.md", logEntry, (err) => {
-      if (err) {
-        console.error("Error writing to log file:", err);
-      } else {
-        console.log("You don't have correct information to access the files.");
-      }
-    });
-
-    const responseHtml = await fs.readFile("./response.html", "utf-8");
-    res.status(200).send(responseHtml);
-  } catch (error) {
-    console.error("Error processing request:", error);
-    res.status(500).send("Error processing request.");
-  }
+ console.log(req.ip)
 });
 
 app.listen(port, () => {
